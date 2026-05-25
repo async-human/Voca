87062ccb078187d855cfed4fc15574a54246418d
async-human <sharshal499@gmail.com>
2026-05-25 18:17:22 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(process.cwd(), 'src'),
+    };
+    return config;
+  },
   async rewrites() {
     return [{ source: '/', destination: '/index.html' }];
   },
