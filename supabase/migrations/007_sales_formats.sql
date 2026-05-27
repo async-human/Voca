af094d63cbb136e30fa7aca515f02eaf1e126aa7
async-human <sharshal499@gmail.com>
2026-05-27 13:23:26 +0530

    second commit

---
+++ supabase/migrations/007_sales_formats.sql
@@ -0,0 +1,35 @@
+-- Allow the sales domain pack formats introduced by the agentic pipeline.
+
+alter table public.recordings drop constraint if exists recordings_format_check;
+alter table public.recordings
+  add constraint recordings_format_check
+  check (
+    format in (
+      'email',
+      'slack',
+      'report',
+      'linkedin',
+      'journal',
+      'post_call_followup',
+      'crm_note',
+      'voicemail_script',
+      'pipeline_update'
+    )
+  );
+
+alter table public.generations drop constraint if exists generations_format_check;
+alter table public.generations
+  add constraint generations_format_check
+  check (
+    format in (
+      'email',
+      'slack',
+      'report',
+      'linkedin',
+      'journal',
+      'post_call_followup',
+      'crm_note',
+      'voicemail_script',
+      'pipeline_update'
+    )
+  );
